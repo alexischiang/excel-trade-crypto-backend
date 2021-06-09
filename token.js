017c5214f61d29e18a92cb77018bc32097a01bf8
jianghanming <jianghanming@digifinex.com>
2021-06-09 11:46:37 +0800

    feat:update igonre

--- token.js
+++
@@ -1,4 +0,0 @@
-module.export = {
-    secret: 'JealhLjJ8pZtWOXav2o2dRbsQeN2HssJjnBQeZcxcKEcRawSHEjBbWKKrEWPjhP6-----',
-    key: 'GGMxQ6Lt90J4aDqUO6v93oyWpqjsN9r5s8m7SiIBSHw9LRRtPyCc4LEvb7qRnu1B',
-}
